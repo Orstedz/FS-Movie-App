a4626746dd09fcbda43d366368f0fb668676ae24
Minh Pham <giaminh2042004@gmail.com>
2025-03-03 22:30:11 +0700

    feat: enhance CORS configuration to allow specific origins and methods

--- BE/server.js
+++ BE/server.js
@@ -4,7 +4,17 @@ import reviews from "./api/reviews.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5500",
+      "http://127.0.0.1:5500",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/v1/reviews", reviews);
